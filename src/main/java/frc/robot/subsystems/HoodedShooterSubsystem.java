c7d1a3e325a531b84260f52bef666f481a8c7101
AndrewS-123 <shanandrew121@gmail.com>
2023-11-30 16:48:53 -0800

    make the hooded shooter shoot (hooded)

--- src/main/java/frc/robot/subsystems/HoodedShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/HoodedShooterSubsystem.java
@@ -12,16 +12,68 @@ public class HoodedShooterSubsystem extends SubsystemBase {
   private double targetAngle;
   private double targetRPM;
 
+  private final CANSparkMax flywheelMotor;
+  private final CANSparkMax hoodMotor;
+  private final RelativeEncoder m_encoder1;
+  private final RelativeEncoder m_encoder2;
+  private final SparkMaxPIDController m_pidController1;
+  private final SparkMaxPIDController m_pidController2;
+  public static double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, flywheelSpeed, allowedErr;
+
   //this is the constructor, and is run when you create the subsystem
   //
-  public HoodedShooterSubsystem() {}
+  public HoodedShooterSubsystem() {
+
+    //make motor for hood
+    flywheelMotor = new CANSparkMax(1, MotorType.kBrushless);
+    flywheelMotor.restoreFactoryDefaults();
+
+    hoodMotor = new CANsparkMax(1, MotorType.kBrushless);
+    hoodMotor.restoreFactoryDefaults();
+
+    m_encoder1 = flywheelMotor.getEncoder();
+    m_encoder1.setPosition(0);
+
+    m_encoder2 = hoodMotor.getEncoder();
+    m_encoder2.setPosition(0);
+
+    m_pidController1 = flywheelMotor.getPIDcontroller();
+    m_pidController2 = hoodMotor.getPIDcontroller();
+    
+    kP = 0.07; 
+    kI = 1e-4;
+    kD = 0.02; 
+    kIz = 0; 
+    kFF = 0; 
+    kMaxOutput = 1; 
+    kMinOutput = -1;
+    allowedErr = 0.1;
+       
+    m_pidController1.setP(kP);
+    m_pidController1.setI(kI);
+    m_pidController1.setD(kD);
+    m_pidController1.setIZone(kIz);
+    m_pidController1.setFF(kFF);
+    m_pidController1.setOutputRange(kMinOutput, kMaxOutput);
+
+    m_pidController2.setP(kP);
+    m_pidController2.setI(kI);
+    m_pidController2.setD(kD);
+    m_pidController2.setIZone(kIz);
+    m_pidController2.setFF(kFF);
+    m_pidController2.setOutputRange(kMinOutput, kMaxOutput);
+    
+  }
 
 
   //this is the periodic function for the subsystem, and it run every tick
   //this is where the PID should be managed, and updated
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    m_pidController1.setReference(targetRPM, CANSparkMax.ControlType.kVelocity);
+    m_pidController2.setReference(targetAngle, CANSparkMax.ControlType.kPosition);
   }
 
   //this is a funciton where you plug in the angle theta (in radians) to set the angle of the hood
@@ -34,10 +86,39 @@ public class HoodedShooterSubsystem extends SubsystemBase {
     targetRPM = rpm;
   }
 
-  //TODO: this is where you should calculate the ideal angle and velocity for the shooter to use
-  public void calculateAngleVelocity() {
-
+  //calculates best angle for shooter to use
+  public double calcudlateBestAngle(double targetX, double targetY) {
+    double targetAngle = Math.atan(targetY/targetX);
+    
+    for(double angle = targetAngle; ; angle += 0.01 * Math.PI / 180) {
+      double velocity = calculateAngleVelocity(targetX, targetY, angle);
+      if(MpsToRPM(velocity) < Constants.maxFlywheelRPM) {
+        return angle;
+      }
+    }
   }
+
+  //calculates velocity for the shooter to use
+  public double calculateAngleVelocity(double targetX, double targetY, double targetAngle) {
+    double x = targetX; 
+    double y = targetY; 
+    double theta = targetAngle; 
+    double g = Constants.gravity;
+    // use our magical formula
+    return Math.sqrt(
+      -x*x*g / (
+        (y - x * Math.tan(theta)) *
+        (2 * Math.cos(theta) * Math.cos(theta))
+      )
+    );
+  }
+  public RelativeEncoder getFlywheelEncoder() {
+    return m_encoder1;
+  }
+  public RelativeEncoder getHoodEncoder() {
+    return m_encoder2;
+  }
+
 
   //helper function to convert meters per second to rotations per minute
   public double MpsToRPM(double mps) {
